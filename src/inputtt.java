4ece92692c891bfdd67358730eef18a0e5deba65
AJITJAIN <sunnyzen00@gmail.com>
2021-06-07 02:26:57 +0530

    initial commit

---
+++ src/inputtt.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class inputtt {
+    public static void main(String[] args) {
+        int sum=0;
+        Scanner sc =new Scanner (System.in);
+        System.out.print("Enter any number You to sum:" );
+        int n =sc.nextInt();
+
+        while(n>0){
+            sum=sum+n;
+            n--;
+            System.out.println("Sum :" +sum);
+
+        }
+    }
+
+}
